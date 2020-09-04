52437d5666851e175bc0ddc98f80e9484e5e05cb
RSFlores <iamrsf0795@gmail.com>
2020-09-04 08:39:11 +0800

    Changelog: + Fixed bug where attached file(s) does not attach to the corresponding Job Request

--- controllers/jobRequests.js
+++ controllers/jobRequests.js
@@ -5,22 +5,22 @@ const Misc = require("../models/misc");
 exports.getRequestNumber = async (req, res) => {
   try {
     if (req.xhr) {
-      const jobRequestNumber = await JobRequest.getJobRequestNumber()
+      const jobRequestNumber = await JobRequest.getJobRequestNumber();
       return res.send(jobRequestNumber);
     }
   } catch (error) {
     console.log(error);
-  }  
+  }
 };
- 
+
 exports.getJobRequestsForAttachment = async (req, res) => {
   if (req.params.option === "Attachment") {
     const jobRequestList = {
-      data: await JobRequest.getJobRequestsForAttachment()
+      data: await JobRequest.getJobRequestsForAttachment(),
     };
     return res.send(jobRequestList);
   }
-}
+};
 
 exports.getJobRequests = async (req, res) => {
   try {
@@ -29,23 +29,28 @@ exports.getJobRequests = async (req, res) => {
     }
     if (req.xhr) {
       const jobRequestList = {
-        data: await JobRequest.getJobRequestByUserAndStatus(req.query.personnelID, req.query.statusID)
+        data: await JobRequest.getJobRequestByUserAndStatus(
+          req.query.personnelID,
+          req.query.statusID
+        ),
       };
       return res.send(jobRequestList);
     }
     return res.render("jobs/job-requests", {
       pageTitle: "Job Requests",
       path: "/jobs/job-requests",
-      assignedCategoriesLink: await Misc.getAssignedCategoriesLink(req.session.assignedCategoryID),
+      assignedCategoriesLink: await Misc.getAssignedCategoriesLink(
+        req.session.assignedCategoryID
+      ),
       jobName: "Request",
       user: await User.getUser("PersonnelID", req.session.personnelID),
-      profileImage:  (profileImage) => {
+      profileImage: (profileImage) => {
         if (!profileImage) {
-          return "/img/user-default.png"
+          return "/img/user-default.png";
         } else {
           return profileImage;
-        } 
-      }
+        }
+      },
     });
   } catch (error) {
     console.log(error);
@@ -55,15 +60,21 @@ exports.getJobRequests = async (req, res) => {
 exports.getJobRequest = async (req, res) => {
   if (req.xhr) {
     return res.send({
-      jobRequestList: await JobRequest.getJobRequest("RequestNumber", req.query.requestNumber),
+      jobRequestList: await JobRequest.getJobRequest(
+        "RequestNumber",
+        req.query.requestNumber
+      ),
       personnelID: req.session.personnelID,
-      attachedFile: await Misc.getAttachedFile("JobRequest", req.query.requestNumber)
+      attachedFile: await Misc.getAttachedFile(
+        "JobRequest",
+        req.query.requestNumber
+      ),
     });
   }
   if (req.query.requestNumber) {
     return res.render("jobs/job-request", {
       pageTitle: "Job Request",
-      path: "/jobs/job-request"
+      path: "/jobs/job-request",
     });
   }
 };
@@ -72,28 +83,28 @@ exports.postDeleteJobRequest = async (req, res) => {
   const request = req.body;
   await JobRequest.deleteJobRequest(request.requestId);
   return res.redirect("/jobs/job-requests");
-}; 
+};
 
 exports.postJobRequests = async (req, res) => {
-  const request = req.body; 
+  const request = req.body;
   const jobRequest = new JobRequest(
     request.requestId || null,
-    request.requestNumber, 
-    request.contactNo, 
-    request.subject, 
+    request.requestNumber,
+    request.contactNo,
+    request.subject,
     request.description,
-    request.dateSent, 
-    request.dateApproved, 
+    request.dateSent,
+    request.dateApproved,
     req.session.personnelID,
-    request.requestedBy,  
+    request.requestedBy,
     request.approvedBy,
-    request.jobStatus  
+    request.jobStatus
   );
-  await jobRequest.insertJobRequest(req.session.personnelID); 
+  await jobRequest.insertJobRequest(req.session.personnelID);
+  const jobRequestNo = await JobRequest.getJobRequestNumber();
   await jobRequest.sendEmailForJobRequest(request.requestedByName);
-  await Misc.insertFiles(request.requestNumber, null, req.files["file-jobRequest"]);
+  await Misc.insertFiles(jobRequestNo[0].JobRequestNumber, null, req.files["file-jobRequest"]);
   // req.rawHeaders[25].split("statusID=")[1]
   // Misc.insertFile(request.requestNumber, null, file.originalname, file.filename, file.mimetype, file.path)
-  return res.redirect("/jobs/job-requests");  
-};    
-  
+  return res.redirect("/jobs/job-requests");
+};
